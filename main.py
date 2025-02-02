c0cd02ee3bff439e0a01ecb59d5db6c117ee9ae4
Mansur <mensurowary@gmail.com>
2025-02-02 16:50:45 -0500

    chore: refactoring

--- main.py
+++ main.py
@@ -64,8 +64,6 @@ for page in [HTML_TEMPLATE, SINGLE_POST_TEMPLATE, INDEX_PAGE_TEMPLATE]:
 if not BUILD_DIR_PATH.exists():
     os.makedirs(BUILD_DIR_PATH, exist_ok=True)
 
-posts_metadata = []
-
 def hilite_code(match):
     lang = match.group(1)
     code = match.group(2)
@@ -78,92 +76,12 @@ def hilite_code(match):
     lexer = get_lexer_by_name(lang, stripall=True)
     return highlight(code, lexer, PYGMENTS_HTML_FORMATTER)
 
-with open("./css/main.scss") as scss_file, open(BUILD_DIR_PATH.joinpath("main.css"), "w+") as main_css:
-    raw_scss = scss_file.read().strip()
-
-    syntax_highligher_styling: str = PYGMENTS_HTML_FORMATTER.get_style_defs()
-
-    for tag_name in STANDARD_TYPES.values():
-        find = f".{tag_name} {{"
-        replace = f"x{tag_name} {{"
-        syntax_highligher_styling = syntax_highligher_styling.replace(find, replace)
-
-    syntax_highligher_styling = f"""
-    .highlight {{
-        {syntax_highligher_styling}
-    }}
-    """
-
-    raw_scss += syntax_highligher_styling
-
-    compiled_css = sass.compile(string=raw_scss, output_style="compressed")
-
-    main_css.write(compiled_css)
-    main_css.flush()
-
 def conditional_render(variable_exists):
     def replace(match):
         if variable_exists:
             return match.group(1)
         return ""
     return replace
-
-# build the files
-for markdown_file in glob.glob("./posts/*.md"):
-    filename = pathlib.Path(markdown_file).name[:-3]
-    os.makedirs(BUILD_DIR_PATH.joinpath("posts", filename), exist_ok=True)
-    with open(markdown_file, encoding="utf-8") as f, open(
-        BUILD_DIR_PATH.joinpath("posts", filename, "index.html"), "w+", encoding="utf-8"
-    ) as o:
-        out_html = MARKDOWN.convert(f.read())
-
-        out_html = re.sub(CODE_EXTRACTION_REGEX, hilite_code, out_html, 0, re.MULTILINE)
-
-        metadata_end_ix = out_html.find("-->")
-        post_metadata = {"filename": f"{filename}"}
-        if metadata_end_ix != -1:
-            metadata = out_html[4:metadata_end_ix]
-            for meta in metadata.splitlines():
-                if not meta.strip():
-                    continue
-
-                key, value = meta.strip().split("=", maxsplit=1)
-                key, value = key.strip(), value.strip()
-                post_metadata[key] = value
-
-            posts_metadata.append(post_metadata)
-
-            out_html = out_html[metadata_end_ix + 3 :]
-        out_html = SINGLE_POST_TEMPLATE.replace("{{CONTENT}}", out_html)
-        out_html = HTML_TEMPLATE.replace("{{CONTENT}}", out_html)
-
-        for key, value in post_metadata.items():
-            if key == "date":
-                value = value[:value.find("T")]
-            out_html = out_html.replace(f"{{{{{key}}}}}", value)
-
-        for cvar in conditional_variables:
-            regex = f"{{{{exists:{cvar}}}}}((.|\\n)+?){{{{exists:{cvar}:end}}}}"
-            out_html = re.sub(regex, conditional_render(cvar in post_metadata), out_html, 0, re.MULTILINE)
-
-        o.write(out_html)
-        o.flush()
-
-# move the static assets
-for file in glob.glob("./assets/*"):
-    filename = pathlib.Path(file).name
-    if filename == "favicon.png":
-        img = Image.open(file)
-        img.save(BUILD_DIR_PATH.joinpath("favicon.ico"), format='ICO', optimize=True)
-    elif filename.endswith(".png"):
-        img = Image.open(file)
-        img = img.convert("RGB")
-        new_filename = filename.replace(".png", ".jpeg")
-        img.save(BUILD_DIR_PATH.joinpath(new_filename), optimize=True)
-    else:
-        # copy everything else as-is
-        shutil.copy2(file, BUILD_DIR_PATH.joinpath(filename))
-
 
 def build_posts(all_posts: list):
     all_posts.sort(key=lambda p: datetime.strptime(p["date"], "%Y-%m-%dT%H:%M:%S%z"), reverse=True)
@@ -180,7 +98,6 @@ def build_posts(all_posts: list):
     output += "</ul>"
     return output
 
-
 def build_projects(projects):
     output = "<ul>"
     for project in projects:
@@ -191,21 +108,114 @@ def build_projects(projects):
 
     return output
 
-# construct index.html
-with open(BUILD_DIR_PATH.joinpath("index.html"), "w+", encoding="utf-8") as o:
-    print("generation started...")
+def handle_css():
+    # handle CSS
+    with open("./css/main.scss") as scss_file, open(BUILD_DIR_PATH.joinpath("main.css"), "w+") as main_css:
+        raw_scss = scss_file.read().strip()
 
-    with open("data.json", encoding="utf-8") as df:
-        config_data = json.load(df)
+        syntax_highligher_styling: str = PYGMENTS_HTML_FORMATTER.get_style_defs()
 
-    posts = build_posts(posts_metadata)
-    projects = build_projects(config_data["all_projects"])
-    index_html = INDEX_PAGE_TEMPLATE.replace("{{POSTS}}", posts).replace(
-        "{{PROJECTS}}", projects
-    )
-    out_html = HTML_TEMPLATE.replace("{{CONTENT}}", index_html).replace(
-        "{{title}}", config_data["blog_title"]
-    )
-    o.write(out_html)
-    o.flush()
-    print("generation done")
+        for tag_name in STANDARD_TYPES.values():
+            find = f".{tag_name} {{"
+            replace = f"x{tag_name} {{"
+            syntax_highligher_styling = syntax_highligher_styling.replace(find, replace)
+
+        syntax_highligher_styling = f"""
+        .highlight {{
+            {syntax_highligher_styling}
+        }}
+        """
+
+        raw_scss += syntax_highligher_styling
+
+        compiled_css = sass.compile(string=raw_scss, output_style="compressed")
+
+        main_css.write(compiled_css)
+        main_css.flush()
+
+def handle_static_assets():
+    # move the static assets
+    for file in glob.glob("./assets/*"):
+        filename = pathlib.Path(file).name
+        if filename == "favicon.png":
+            img = Image.open(file)
+            img.save(BUILD_DIR_PATH.joinpath("favicon.ico"), format='ICO', optimize=True)
+        elif filename.endswith(".png"):
+            img = Image.open(file)
+            img = img.convert("RGB")
+            new_filename = filename.replace(".png", ".jpeg")
+            img.save(BUILD_DIR_PATH.joinpath(new_filename), optimize=True)
+        else:
+            # copy everything else as-is
+            shutil.copy2(file, BUILD_DIR_PATH.joinpath(filename))
+
+def handle_posts():
+    posts_metadata = []
+    # build the files
+    for markdown_file in glob.glob("./posts/*.md"):
+        filename = pathlib.Path(markdown_file).name[:-3]
+        os.makedirs(BUILD_DIR_PATH.joinpath("posts", filename), exist_ok=True)
+        with open(markdown_file, encoding="utf-8") as f, open(
+            BUILD_DIR_PATH.joinpath("posts", filename, "index.html"), "w+", encoding="utf-8"
+        ) as o:
+            out_html = MARKDOWN.convert(f.read())
+
+            out_html = re.sub(CODE_EXTRACTION_REGEX, hilite_code, out_html, 0, re.MULTILINE)
+
+            metadata_end_ix = out_html.find("-->")
+            post_metadata = {"filename": f"{filename}"}
+            if metadata_end_ix != -1:
+                metadata = out_html[4:metadata_end_ix]
+                for meta in metadata.splitlines():
+                    if not meta.strip():
+                        continue
+
+                    key, value = meta.strip().split("=", maxsplit=1)
+                    key, value = key.strip(), value.strip()
+                    post_metadata[key] = value
+
+                posts_metadata.append(post_metadata)
+
+                out_html = out_html[metadata_end_ix + 3 :]
+            out_html = SINGLE_POST_TEMPLATE.replace("{{CONTENT}}", out_html)
+            out_html = HTML_TEMPLATE.replace("{{CONTENT}}", out_html)
+
+            for key, value in post_metadata.items():
+                if key == "date":
+                    value = value[:value.find("T")]
+                out_html = out_html.replace(f"{{{{{key}}}}}", value)
+
+            for cvar in conditional_variables:
+                regex = f"{{{{exists:{cvar}}}}}((.|\\n)+?){{{{exists:{cvar}:end}}}}"
+                out_html = re.sub(regex, conditional_render(cvar in post_metadata), out_html, 0, re.MULTILINE)
+
+            o.write(out_html)
+            o.flush()
+
+    return posts_metadata
+
+def construct_index_html(posts_metadata):
+    # construct index.html
+    with open(BUILD_DIR_PATH.joinpath("index.html"), "w+", encoding="utf-8") as o:
+        print("generation started...")
+
+        with open("data.json", encoding="utf-8") as df:
+            config_data = json.load(df)
+
+        posts = build_posts(posts_metadata)
+        projects = build_projects(config_data["all_projects"])
+        index_html = INDEX_PAGE_TEMPLATE.replace("{{POSTS}}", posts).replace(
+            "{{PROJECTS}}", projects
+        )
+        out_html = HTML_TEMPLATE.replace("{{CONTENT}}", index_html).replace(
+            "{{title}}", config_data["blog_title"]
+        )
+        o.write(out_html)
+        o.flush()
+        print("generation done")
+
+if __name__ == "__main__":
+    handle_css()
+    handle_static_assets()
+    posts_metadata = handle_posts()
+    construct_index_html(posts_metadata)
